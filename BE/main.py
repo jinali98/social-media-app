25cdbbce0245f618636b6f920ae8c0bad860828b
jinali98 <jinalipabasara@gmail.com>
2023-10-21 11:37:56 +0530

    project be set up with fast api

---
+++ BE/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def get_user():
+    return {"message": "Hello World!!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"posts": []}
